15999ca4916cd47c7cc64bc940bc1a6f12d8e1fd
Brianhin94 <brianhin64@gmail.com>
2021-01-20 13:26:45 -0700

    hello

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const bcrypt = require('bcrypt');
+
 const {
   Model
 } = require('sequelize');
@@ -12,12 +14,48 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
+    validPassword(typedPassword) {
+      let isValid = bcrypt.compareSync(typedPassword, this.password);
+      return isValid;
+    }
+
+    toJSON() {
+      let userData = this.get();
+      delete userData.password;
+      return userData;
+    }
   };
   user.init({
-    email: DataTypes.STRING,
-    name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'invalid email address'
+        }
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          msg: 'name must be 1-99 characters'
+        }
+      }
+    },
     password: DataTypes.STRING
   }, {
+    hooks: {
+      beforeCreate: (pendingUser, options) => {
+        // check if there is a user being passed AND that user has a pw
+        if (pendingUser && pendingUser.password) {
+          // Hash the pass
+          let hash = bcrypt.hashSync(pendingUser.password, 12);
+          // store the hash as users pw
+          pendingUser.password = hash
+        }
+      }
+    },
     sequelize,
     modelName: 'user',
   });
