e0ee1f2e5ad2ab17dd80b67831407aef88e11e80
Brianhin94 <brianhin64@gmail.com>
2021-01-26 16:10:35 -0700

    habit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const axios = require('axios');
+// const cocktailSearchURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
+// const ingredientSearchURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka`;
 const session = require('express-session');
 const flash = require('connect-flash');
 const helmet = require('helmet');
@@ -11,13 +14,20 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
+// allows us to read form sent body data
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
-app.use(helmet());
+// app.use(helmet());
+// Not good for doing during practice
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET, // should be an ENV variable
   resave: false,
   saveUninitialized: true
 }));
@@ -27,7 +37,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-// Write Custom Middleware to access the user on every response
+// Write custom middleware to access the user on every response
 app.use((req, res, next) => {
   let alerts = req.flash();
   console.log(alerts);
@@ -36,17 +46,83 @@ app.use((req, res, next) => {
   next();
 });
 
+// ROUTES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/profile', isLoggedIn, (req, res) => {
-  res.render('profile');
+app.get('/favorites', isLoggedIn, (req, res) => {
+  res.render('favorites');
 });
 
-app.use('/auth', require('./routes/auth'));
-// app.use('/dino', isLoggedIn, require('./routes/dinos'));
+app.get('/search', (req, res) => {
+  res.render('search')
+});
 
-var server = app.listen(process.env.PORT || 3000, ()=> console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
+// Get route for show
+app.get('/show', (req, res) => {
+  res.render('show')
+});
+
+// Route for searching by cocktail name
+app.post('/search', (req, res) => {
+  console.log('enter function');
+  console.log(req.body.name);
+  const cocktailName = req.body.name
+  if (cocktailName) {
+    const URL2 = `https://www.thecocktaildb.com/api/json/v1/1/search.php?`
+    axios.get(`${URL2}s=${cocktailName}`)
+      .then(response => {
+        console.log('response');
+        // console.log(response);
+        let matchByCocktailName = response.data
+        console.log(matchByCocktailName);
+        res.render('show', { matchByCocktailName: matchByCocktailName })
+      })
+    const ingredientName = req.query.name
+  } else if (ingredientName) {
+    const URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?`
+    axios.get(`${URL}i=${ingredientName}`)
+      .then(response => {
+        let matchByIngredient = response.data
+        res.render('show', {
+          matchByIngredient: matchByIngredient
+        })
+      })
+  }
+});
+
+ // Route for searching by ingredient
+ app.post('/search', (req, res) => {
+ const ingredientName = req.query.name
+ const URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?`
+ axios.get(`${URL}i=${ingredientName}`)
+ .then(response => {
+   let matchByIngredient = response.data
+  res.render('show', {
+    matchByIngredient : matchByIngredient
+  })
+ })
+ });
+
+
+
+// post route 
+app.post('/favorites', (req, res) => {
+  console.log(req.body);
+});
+
+
+// axios.get(cocktailSearchURL)
+//   .then(response => {
+//     console.log(response.data);
+//   }).catch(err => {
+//     console.log(err);
+//   });
+
+app.use('/auth', require('./routes/auth'));
+
+var server = app.listen(process.env.PORT || 3000, () => console.log(`🎧You're listening to the smooth sounds of port ${process.env.PORT || 3000}🎧`));
 
 module.exports = server;
